d9f7d3a273c81f88856cd58c559bd4aedce9d6fc
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-08 07:55:45 +0000

    Uptaded navigation bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,60 @@
-let NavBar = document.createElement("div");
-let NavBarUL = document.createElement("ul");
+let navBar = document.createElement("div");
+navBar.setAttribute("id", "navbar-container")
+
+let navBarUL = document.createElement("ul");
+
+
+// HOME BUTTON
 let homeItem = document.createElement("li");
-let homeAnchor = document.createElement("A")
-homeAnchor.setAttribute("href", "https://www.w3schools.com");
+let homeAnchor = document.createElement('a');
+homeAnchor.setAttribute('href',"http://www.google.com.br");
+homeAnchor.innerHTML = "home";
+homeItem.appendChild(homeAnchor);
+navBarUL.appendChild(homeItem);
+
+// ABOUT BUTTON
+let aboutItem = document.createElement("li");
+let aboutAnchor = document.createElement('a');
+aboutAnchor.setAttribute('href',"http://www.google.com.br");
+aboutAnchor.innerHTML = "about";
+aboutItem.appendChild(aboutAnchor);
+navBarUL.appendChild(aboutItem);
+
+// LOGO
+let logoItem = document.createElement("li");
+let logoAnchor = document.createElement('a');
+aboutAnchor.setAttribute('href',"http://www.google.com.br");
+let navBarImg = document.createElement("img");
+navBarImg.src = "assets/images/logo.png"
+logoAnchor.appendChild(navBarImg);
+logoItem.appendChild(logoAnchor);
+navBarUL.appendChild(logoItem);
+
+
+// BEERS BUTTON
+let beersItem = document.createElement("li");
+let beersAnchor = document.createElement('a');
+beersAnchor.setAttribute('href',"http://www.google.com.br");
+beersAnchor.innerHTML = "beers";
+beersItem.appendChild(beersAnchor);
+navBarUL.appendChild(beersItem);
+
+// CONTACT BUTTON
+let contactItem = document.createElement("li");
+let contactAnchor = document.createElement('a');
+contactAnchor.setAttribute('href',"http://www.google.com.br");
+contactAnchor.innerHTML = "contact";
+contactItem.appendChild(contactAnchor);
+navBarUL.appendChild(contactItem);
+
+
+navBar.appendChild(navBarUL);
 
 
 
 
-NavBar.appendChild(NavBarUL);
-NavBarUL.appendChild(homeItem);
-//homeItem.appendChild(homeAnchor);
-
-homeItem.innerText = "home"
-homeItem.innerHTML = homeAnchor
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.body.appendChild(NavBar);
-
+document.body.appendChild(navBar);
