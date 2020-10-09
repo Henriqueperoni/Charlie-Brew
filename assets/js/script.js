6e83e25717a8c2d9f48b207f5df98ec0bf9e8dce
Henriqueperoni <henriqueperoni@hotmail.com>
2020-10-09 08:22:38 +0000

    Add text to about section and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,6 +65,8 @@ heroDiv.appendChild(introDiv);
 let myH1 = document.createElement("H1");
 introDiv.appendChild(myH1);
 
+
+
 // H1 CONTAINER
 let H1SpanOne = document.createElement("span");
 let H1SpanTwo = document.createElement("span")
@@ -80,6 +82,7 @@ h1Paragrapher.innerHTML = "Where the beer is served colder than your ex's heart"
 introDiv.appendChild(h1Paragrapher);
 
 
+
 // ABOUT HEADING
 let aboutHeadingDiv = document.createElement("div");
 aboutHeadingDiv.setAttribute("class", "containerHeading");
@@ -92,10 +95,44 @@ aboutHeadingDiv.appendChild(aboutHeading);
 let aboutContainer = document.createElement("div");
 aboutContainer.setAttribute("class", "containerAbout")
 
-let aboutContainerText = document.createElement("p");
-aboutContainerText.innerHTML = 'Quoting the best US president in history, Abraham Lincoln "I am a firm believer in the people. If given the truth, they can be depended upon to meet any national crisis. The great point is to bring them the real facts, and beer.” This is one of the main reasons Henrique Peroni created Charlie Brew Brewery in 2032, because as  Mr.s Lincoln, Henrique does believe in people, and in the middle of the chaos, a cold pint of beer can help solve the problems, or at least make you forget then for a while.'
+//ABOUT PARAGRAPHERS
+let paragrapherContainer = document.createElement("div")
+paragrapherContainer.setAttribute("class", "aboutText")
+aboutContainer.appendChild(paragrapherContainer);
+let aboutContainerTextOne = document.createElement("p");
+aboutContainerTextOne.innerHTML = 'Quoting the best US president in history, Abraham Lincoln "I am a firm believer in the people. If given the truth, they can be depended upon to meet any national crisis. The great point is to bring them the real facts, and beer.” This is one of the main reasons Henrique Peroni created Charlie Brew Brewery in 2032, because as  Mr.s Lincoln, Henrique does believe in people, and in the middle of the chaos, a cold pint of beer can help solve the problems, or at least make you forget then for a while.'
+paragrapherContainer.appendChild(aboutContainerTextOne);
 
-aboutContainer.appendChild(aboutContainerText);
+let aboutContainerTextTwo = document.createElement("p");
+aboutContainerTextTwo.innerHTML = "All Charlie beers are brewed, bottled, canned and kegged at the north of the magic island of Florianopolis. All the ingredients such as barley and hop come from the local community, which helps the people around the Charlie Brew and keep the high quality of these unique beers.  Each beer has its own distinct name, story, and taste experience. Discover our beers. Discover our stories!"
+paragrapherContainer.appendChild(aboutContainerTextTwo);
+
+
+// DIV IMAGES + TEXT
+let containerHistory = document.createElement("div");
+containerHistory.setAttribute("class", "aboutHistory");
+aboutContainer.appendChild(containerHistory);
+
+let pubImageDiv = document.createElement("div");
+pubImageDiv.setAttribute("class", "aboutContent");
+containerHistory.appendChild(pubImageDiv)
+let pubImage = document.createElement("img");
+pubImage.src = "assets/images/pub-inside.png";
+pubImageDiv.appendChild(pubImage);
+
+let pubTextDiv = document.createElement("div");
+pubTextDiv.setAttribute("class", "aboutContent");
+containerHistory.appendChild(pubTextDiv);
+let pubImageDivH3 = document.createElement("h3");
+pubImageDivH3.innerHTML = "pub";
+pubTextDiv.appendChild(pubImageDivH3);
+let pubImageDivParagrapher = document.createElement("p");
+pubImageDivParagrapher.innerHTML = "hfdisjdifodsjfodmsofidj ofj odo ofi doifj oijf dj ojdo jfoid jfoidjoif dsodi osdjfodjofjdoi ojdiojdiosjiodjfo sdoif odijf oisdj ifo";
+pubTextDiv.appendChild(pubImageDivParagrapher);
+
+
+
+
 
 
 // APPEDING DIVS
